e039362c9f191b334c8bb41533582d146d5f4182
Allison Cristiano Lindner <lindner.allison@gmail.com>
2016-10-31 16:04:22 -0200

    Prototipo
    Prototipo Inicial

---
+++ MarioNaoMario/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehavior : MonoBehaviour {
+
+	public GameObject player;
+
+	void Start() {
+	}
+
+	void LateUpdate() {
+		Vector3 translate = new Vector3(	Camera.main.transform.position.x,
+											player.transform.position.y,
+											0.0f) - Camera.main.transform.position;
+		translate *= Time.deltaTime;
+		translate.z = 0.0f;
+		Camera.main.transform.Translate(translate);
+	}
+}
